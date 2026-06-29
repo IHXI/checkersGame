bd39cd64223796a3ad6fd67b71e9ea323b6f54b4
Hawra <hawraasharaf89@gmail.com>
2026-06-29 16:06:39 +0300

    fixes pieces

--- app.js
+++ app.js
@@ -1,31 +1,59 @@
+
 const squares = document.querySelectorAll('.sqr')
-const boardRow1 = document.querySelector('#row1') 
-const boardRow2 = document.querySelector('#row2') 
-const boardRow3 = document.querySelector('#row3') 
-const boardRow4 = document.querySelector('#row4') 
-const boardRow5 = document.querySelector('#row5') 
-const boardRow6 = document.querySelector('#row6') 
-const boardRow7 = document.querySelector('#row7') 
-const boardRow8 = document.querySelector('#row8') 
+const boardRow1 = document.getElementById('101') 
+const boardRow2 = document.getElementById('102') 
+const boardRow3 = document.getElementById('103') 
+const boardRow4 = document.getElementById('104') 
+const boardRow5 = document.getElementById('105') 
+const boardRow6 = document.getElementById('106') 
+const boardRow7 = document.getElementById('107') 
+const boardRow8 = document.getElementById('108') 
+const whitePawn = document.querySelectorAll('.white-pawn')
 
 let clicked = false
 
 squares.forEach(function(element){
     if(element.id % 2 && boardRow1.contains(element) || element.id % 2 ===0 && boardRow2.contains(element) || element.id % 2 && boardRow3.contains(element) || element.id % 2 === 0 && boardRow4.contains(element) || element.id % 2 && boardRow5.contains(element) || element.id % 2 === 0 && boardRow6.contains(element) || element.id % 2 && boardRow7.contains(element) || element.id % 2 === 0 && boardRow8.contains(element)){
-        element.style.backgroundColor = 'wheat'
-        console.log(element)}
+        element.style.backgroundColor = 'wheat'}
 
     if(element.id % 2 === 0 && boardRow1.contains(element) || element.id % 2 && boardRow2.contains(element) || element.id % 2 === 0 && boardRow3.contains(element) || element.id % 2 && boardRow4.contains(element) || element.id % 2 === 0 && boardRow5.contains(element) || element.id % 2 && boardRow6.contains(element) || element.id % 2 === 0 && boardRow7.contains(element) || element.id % 2 && boardRow8.contains(element)){
-        element.style.backgroundColor = 'saddlebrown'
-        console.log(element)}
+        element.style.backgroundColor = 'saddlebrown'}
     
-    if(element.addEventListener('click', function(){
-        console.log(element.id)
-        let blackPawn = document.createElement('img')
-        blackPawn.src = '/assets/black-pawn.jpg'
-        element.appendChild(blackPawn)
-        clicked = true   
-    })){}
+
+
+   element.addEventListener('click', function(){
+    console.log(element.id)
+    if (element.children[0].classList[0] === 'white-pawn') {
+        const image = document.createElement('img')
+        image.src = '/assets/green-dot.png'
+        image.classList = 'green-dot'
         
+        let currentSquare = Number(element.id) 
+
+        console.log(currentSquare)
+        let move = document.getElementById(currentSquare + 7)
+        move.appendChild(image)
+
+        let move2 = document.getElementById(currentSquare + 9)
+        move2.appendChild(image)
+        console.log(move)
+        console.log(move2)
+
+    }
+
+
+
+
+    if (element.children[0].classList[0] === 'black-pawn') {
+        element.removeChild(element.firstChild)
+        console.log(currentSquare)
+        let move = document.getElementById(currentSquare -= 9)
+        move.appendChild(image)
+
+    }
+
 
     })
+    
+
+})
